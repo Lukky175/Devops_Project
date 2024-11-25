7908c3460ea63cfd61514d17709e083ea75deb2d
Lukky175 <131709990+Lukky175@users.noreply.github.com>
2024-11-25 12:39:28 +0530

    Update Disk_Utilisation.sh

--- ShellScripts_For_Pipeline/Disk_Utilisation.sh
+++ ShellScripts_For_Pipeline/Disk_Utilisation.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-# Get the disk usage percentage for the root directory
-disk_usage=$(df / | grep / | awk '{ print $5 }' | sed 's/%//g')
+THRESHOLD=80
+# Use grep to filter the '/' line and awk to extract the usage percentage
+USAGE=$(df -h | grep ' /$' | awk '{print $5}' | awk '{print substr($0, 1, length($0)-1)}') # Removes '%'
 
-# Check if the disk usage is greater than or equal to 80%
-if [ "$disk_usage" -ge 80 ]; then
-    # Send an email alert if the disk usage exceeds the threshold
-    echo "Disk usage is critically high: ${disk_usage}% on $(hostname)" | mail -s "Disk Utilization Alert - ${disk_usage}%" lakshit175@gmail.com
+if [ "$USAGE" -gt "$THRESHOLD" ]; then
+    echo "Disk usage critical: ${USAGE}% (Threshold: ${THRESHOLD}%)"
+    exit 1 # Non-zero exit code for critical disk usage
+else
+    echo "Disk usage is under control: ${USAGE}% (Threshold: ${THRESHOLD}%)"
+    exit 0 # Zero exit code for normal usage
 fi
