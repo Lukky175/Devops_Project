011b6bef727bc74819a62888af50f0c758bf85fa
Lukky175 <131709990+Lukky175@users.noreply.github.com>
2024-11-25 12:41:30 +0530

    Update Disk_Utilisation.sh

--- ShellScripts_For_Pipeline/Disk_Utilisation.sh
+++ ShellScripts_For_Pipeline/Disk_Utilisation.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-THRESHOLD=10
+THRESHOLD=2
 # Use grep to filter the '/' line and awk to extract the usage percentage
 USAGE=$(df -h | grep ' /$' | awk '{print $5}' | awk '{print substr($0, 1, length($0)-1)}') # Removes '%'
 
