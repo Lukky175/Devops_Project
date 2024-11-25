1c1d1773b9b81ac9eea7098965811e750243ea04
Lukky175 <131709990+Lukky175@users.noreply.github.com>
2024-11-25 12:13:16 +0530

    Update Disk_Utilisation.sh

--- ShellScripts_For_Pipeline/Disk_Utilisation.sh
+++ ShellScripts_For_Pipeline/Disk_Utilisation.sh
@@ -6,5 +6,5 @@ disk_usage=$(df / | grep / | awk '{ print $5 }' | sed 's/%//g')
 # Check if the disk usage is greater than or equal to 80%
 if [ "$disk_usage" -ge 80 ]; then
     # Send an email alert if the disk usage exceeds the threshold
-    echo "Disk usage is critically high: ${disk_usage}% on $(hostname)" | mail -s "Disk Utilization Alert - ${disk_usage}%" jayapramodmanikantan@gmail.com
+    echo "Disk usage is critically high: ${disk_usage}% on $(hostname)" | mail -s "Disk Utilization Alert - ${disk_usage}%" lakshit175@gmail.com
 fi
